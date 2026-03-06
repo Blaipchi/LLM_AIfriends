a845d64327906a27feca93e05bff2c7b0e470d67
blaipchi <1684869595@qq.com>
2026-03-06 16:37:57 +0800

    定义User数据库并且同步到db.sqlite3中

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from web.models.user import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    # 这里的作用主要是，避免打开页面的时候直接卡死的问题
+    raw_id_fields = ('user',) # 逗号千万不能删！！
